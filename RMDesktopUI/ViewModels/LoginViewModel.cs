177c62b3a2e2402864fd38496b7c88d1c8e6891b
Santiago Gerhart <gerhartsantiago@gmail.com>
2020-11-30 23:04:19 -0300

    Login Error Handler

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -41,6 +41,32 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
+        public bool IsErrorVisible
+        {
+            get 
+            {
+                bool output = false;
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output; 
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set 
+            { 
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => ErrorMessage);
+                NotifyOfPropertyChange(() => IsErrorVisible);
+            }
+        }
+
         public bool CanLogIn
         {
             get
@@ -59,11 +85,12 @@ namespace RMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = string.Empty;
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }        
         }
     }
